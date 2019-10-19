020bc23b836f880e9d274edad06a6c011339397d
“Peter <2296505z@student.gla.ac.uk>
2019-10-19 13:12:49 +0100

    distance function impemented

--- GoalCamper/mathfuncs.py
+++ GoalCamper/mathfuncs.py
@@ -8,3 +8,6 @@ def getHeading(x,y,X,Y):
     heading = RadToDeg(heading)
     heading = (360 - heading) % 360
     return abs(heading)
+
+def dist(x1,y1,x2,y2):
+	return sqrt((x1 + x2)**2 + (y1 + y2)**2)
