104833727e65379d18ca0207d77bc6dd07472d9f
jaroslavp <jarko.paska@gmail.com>
2019-10-19 19:50:35 +0100

    goodbot init weights

--- BadBot/main.py
+++ BadBot/main.py
@@ -17,7 +17,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--debug', action='store_true', help='Enable debug output')
 parser.add_argument('-H', '--hostname', default='127.0.0.1', help='Hostname to connect to')
 parser.add_argument('-p', '--port', default=8052, type=int, help='Port to connect to')
-parser.add_argument('-n', '--name', default='MSThanks:', help='Name of bot')
+parser.add_argument('-n', '--name', default='BadBot:', help='Name of bot')
 args = parser.parse_args()
 
 # Set up console logging
